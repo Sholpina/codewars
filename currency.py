c3924b3dfea1427d45312ebc5232352322d91b38
Sholpina <30983083+Sholpina@users.noreply.github.com>
2022-10-28 00:48:33 +0400

    Uppdate file

--- currency.py
+++ currency.py
@@ -1,10 +1,10 @@
 def solution(to_cur,values):
     if to_cur == "USD":
-        values = list(map (lambda x: x*1.13987654, values))
+        values = list(map (lambda x: x*1.1398734654, values))
         values = ['${:,.2f}'.format(item) for item in values]
         return values
     elif to_cur == "EUR":
-        values = list(map (lambda x: x/1.13987654, values))
+        values = list(map (lambda x: x/1.13987124, values))
         values = ['{:,.2f}€'.format(item) for item in values]
         return values
     else:
